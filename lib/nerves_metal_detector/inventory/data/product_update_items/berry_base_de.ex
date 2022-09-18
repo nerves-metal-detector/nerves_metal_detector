632723367c3ca33f7916b2e533b314882361801a
Phillipp Ohlandt <phillipp.ohlandt@googlemail.com>
2022-09-18 21:34:59 +0200

    add all berrybase.de items for rpi models from rpilocator

--- lib/nerves_metal_detector/inventory/data/product_update_items/berry_base_de.ex
+++ lib/nerves_metal_detector/inventory/data/product_update_items/berry_base_de.ex
@@ -2,6 +2,7 @@ defmodule NervesMetalDetector.Inventory.Data.ProductUpdateItems.BerryBaseDe do
   alias NervesMetalDetector.Vendors.BerryBaseDe
 
   @items [
+    # RPi 4
     %BerryBaseDe.ProductUpdate{
       url: "https://www.berrybase.de/raspberry-pi-4-computer-modell-b-1gb-ram",
       sku: "RPI4-MODBP-1GB"
@@ -17,6 +18,160 @@ defmodule NervesMetalDetector.Inventory.Data.ProductUpdateItems.BerryBaseDe do
     %BerryBaseDe.ProductUpdate{
       url: "https://www.berrybase.de/raspberry-pi-4-computer-modell-b-8gb-ram",
       sku: "RPI4-MODBP-8GB"
+    },
+
+    # RPi CM3
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-3-b-lite",
+      sku: "CM3+Lite"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-3-b-8gb",
+      sku: "CM3+8GB"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-3-b-16gb",
+      sku: "CM3+16GB"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-3-b-32gb",
+      sku: "CM3+32GB"
+    },
+
+    # RPi CM4
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-2gb-ram-32gb-flash-wlan-bt",
+      sku: "CM4102032"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-8gb-ram-16gb-flash",
+      sku: "CM4008016"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-8gb-ram-lite",
+      sku: "CM4008000"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-4gb-ram-32gb-flash",
+      sku: "CM4004032"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-4gb-ram-lite",
+      sku: "CM4004000"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-2gb-ram-32gb-flash",
+      sku: "CM4002032"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-2gb-ram-16gb-flash",
+      sku: "CM4002016"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-2gb-ram-8gb-flash",
+      sku: "CM4002008"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-2gb-ram-lite",
+      sku: "CM4002000"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-1gb-ram-32gb-flash",
+      sku: "CM4001032"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-1gb-ram-16gb-flash",
+      sku: "CM4001016"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-1gb-ram-8gb-flash",
+      sku: "CM4001008"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-8gb-ram-8gb-flash",
+      sku: "CM4008008"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-8gb-ram-32gb-flash-wlan-bt",
+      sku: "CM4108032"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-2gb-ram-8gb-flash-wlan-bt",
+      sku: "CM4102008"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-4gb-ram-16gb-flash-wlan-bt",
+      sku: "CM4104016"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-4gb-ram-8gb-flash",
+      sku: "CM4004008"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-4gb-ram-8gb-flash-wlan-bt",
+      sku: "CM4104008"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-4gb-ram-32gb-flash-wlan-bt",
+      sku: "CM4104032"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-1gb-ram-lite",
+      sku: "CM4001000"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-2gb-ram-16gb-flash-wlan-bt",
+      sku: "CM4102016"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-8gb-ram-16gb-flash-wlan-bt",
+      sku: "CM4108016"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-8gb-ram-8gb-flash-wlan-bt",
+      sku: "CM4108008"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-8gb-ram-lite-wlan-bt",
+      sku: "CM4108000"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-4gb-ram-lite-wlan-43-bt",
+      sku: "CM4104000"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-2gb-ram-lite-wlan-bt",
+      sku: "CM4102000"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-1gb-ram-32gb-flash-wlan-bt",
+      sku: "CM4101032"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-1gb-ram-16gb-flash-wlan-bt",
+      sku: "CM4101016"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-1gb-ram-8gb-flash-wlan-bt",
+      sku: "CM4101008"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-1gb-ram-lite-wlan-bt",
+      sku: "CM4101000"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-8gb-ram-32gb-flash",
+      sku: "CM4008032"
+    },
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-compute-module-4-4gb-ram-16gb-flash",
+      sku: "CM4004016"
+    },
+
+    # RPi Zero 2
+    %BerryBaseDe.ProductUpdate{
+      url: "https://www.berrybase.de/raspberry-pi-zero-2-w",
+      sku: "SC0510"
     }
   ]
 
