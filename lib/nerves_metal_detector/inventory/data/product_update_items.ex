0c6c273644b863bed1579ad1b1bef8e9aa5cf9b6
Ben <bflatau@gmail.com>
2022-10-11 23:28:41 -0700

    missed some file cleanups...hopefully ready for PR..

--- lib/nerves_metal_detector/inventory/data/product_update_items.ex
+++ lib/nerves_metal_detector/inventory/data/product_update_items.ex
@@ -2,22 +2,22 @@ defmodule NervesMetalDetector.Inventory.Data.ProductUpdateItems do
   alias NervesMetalDetector.Inventory.Data.ProductUpdateItems
 
   @collections [
-    # ProductUpdateItems.AdafruitUs,
-    # ProductUpdateItems.BerryBaseDe,
-    # ProductUpdateItems.BuyzeroDe,
-    # ProductUpdateItems.KubiiFr,
-    # ProductUpdateItems.McHobbyBe,
-    # ProductUpdateItems.MeloperoIt,
-    # ProductUpdateItems.PimoroniUk,
-    # ProductUpdateItems.PiShopCa,
-    # ProductUpdateItems.PiShopCh,
-    # ProductUpdateItems.PiShopUs,
-    # ProductUpdateItems.RasppishopDe,
-    # ProductUpdateItems.ReicheltDe,
-    # ProductUpdateItems.SeeedStudioCn,
-    # ProductUpdateItems.SemafAt,
-    # ProductUpdateItems.TiendatecEs,
-    # ProductUpdateItems.WelectronDe,
+    ProductUpdateItems.AdafruitUs,
+    ProductUpdateItems.BerryBaseDe,
+    ProductUpdateItems.BuyzeroDe,
+    ProductUpdateItems.KubiiFr,
+    ProductUpdateItems.McHobbyBe,
+    ProductUpdateItems.MeloperoIt,
+    ProductUpdateItems.PimoroniUk,
+    ProductUpdateItems.PiShopCa,
+    ProductUpdateItems.PiShopCh,
+    ProductUpdateItems.PiShopUs,
+    ProductUpdateItems.RasppishopDe,
+    ProductUpdateItems.ReicheltDe,
+    ProductUpdateItems.SeeedStudioCn,
+    ProductUpdateItems.SemafAt,
+    ProductUpdateItems.TiendatecEs,
+    ProductUpdateItems.WelectronDe,
     ProductUpdateItems.SparkfunUs
   ]
 
