d564fe8684c2b9532f6d9df9ebcabf7419b0807b
Phillipp Ohlandt <phillipp.ohlandt@googlemail.com>
2023-01-30 22:48:28 +0100

    add BotlandPl vendor and its products from rpilocator + the same for BotlandDe, BotlandCz and BotlandEu

---
+++ lib/nerves_metal_detector/inventory/data/product_update_items/botland_pl.ex
@@ -0,0 +1,172 @@
+defmodule NervesMetalDetector.Inventory.Data.ProductUpdateItems.BotlandPl do
+  alias NervesMetalDetector.Vendors.BotlandPl.ProductUpdate
+
+  @items [
+    # RPi CM4
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18193-raspberry-pi-cm4-compute-module-4-2gb-ram-32gb-emmc-wifibluetooth-cm4102032-5904422368470.html",
+      sku: "CM4102032"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18179-raspberry-pi-cm4-compute-module-4-1gb-ram-8gb-emmc-cm4001008-5904422368425.html",
+      sku: "CM4001008"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18202-raspberry-pi-cm4-compute-module-4-4gb-ram-32gb-emmc-wifibluetooth-cm4104032-5904422368531.html",
+      sku: "CM4104032"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18192-raspberry-pi-cm4-compute-module-4-2gb-ram-16gb-emmc-wifibluetooth-cm4102016-5904422368456.html",
+      sku: "CM4102016"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18191-raspberry-pi-cm4-compute-module-4-2gb-ram-8gb-emmc-wifibluetooth-cm4102008-5904422368494.html",
+      sku: "CM4102008"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18178-raspberry-pi-cm4-lite-compute-module-4-1gb-ram-cm4001000-5904422368623.html",
+      sku: "CM4001000"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18180-raspberry-pi-cm4-compute-module-4-1gb-ram-16gb-emmc-cm4001016-5904422368388.html",
+      sku: "CM4001016"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18181-raspberry-pi-cm4-compute-module-4-1gb-ram-32gb-emmc-cm4001032-5904422368401.html",
+      sku: "CM4001032"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18186-raspberry-pi-cm4-lite-compute-module-4-2gb-ram-cm4002000-5904422368647.html",
+      sku: "CM4002000"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18187-raspberry-pi-cm4-compute-module-4-2gb-ram-8gb-emmc-cm4002008-5904422368487.html",
+      sku: "CM4002008"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18188-raspberry-pi-cm4-compute-module-4-2gb-ram-16gb-emmc-cm4002016-5904422368449.html",
+      sku: "CM4002016"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18189-raspberry-pi-cm4-compute-module-4-2gb-ram-32gb-emmc-cm4002032-5904422368463.html",
+      sku: "CM4002032"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18194-raspberry-pi-cm4-lite-compute-module-4-4gb-ram-cm4004000-5904422368661.html",
+      sku: "CM4004000"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18195-raspberry-pi-cm4-compute-module-4-4gb-ram-8gb-emmc-cm4004008-5904422368548.html",
+      sku: "CM4004008"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18196-raspberry-pi-cm4-compute-module-4-4gb-ram-16gb-emmc-cm4004016-5904422368500.html",
+      sku: "CM4004016"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18197-raspberry-pi-cm4-compute-module-4-4gb-ram-32gb-emmc-cm4004032-5904422368524.html",
+      sku: "CM4004032"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18204-raspberry-pi-cm4-lite-compute-module-4-8gb-ram-cm4008000-5904422368685.html",
+      sku: "CM4008000"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18205-raspberry-pi-cm4-compute-module-4-8gb-ram-8gb-emmc-cm4008008-5904422368609.html",
+      sku: "CM4008008"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18206-raspberry-pi-cm4-compute-module-4-8gb-ram-16gb-emmc-cm4008016-5904422368562.html",
+      sku: "CM4008016"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18207-raspberry-pi-cm4-compute-module-4-8gb-ram-32gb-emmc-cm4008032-5904422368586.html",
+      sku: "CM4008032"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18182-raspberry-pi-cm4-lite-compute-module-4-1gb-ram-wifibluetooth-cm4101000-5904422368630.html",
+      sku: "CM4101000"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18183-raspberry-pi-cm4-compute-module-4-1gb-ram-8gb-emmc-wifibluetooth-cm4101008-5904422368432.html",
+      sku: "CM4101008"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-cm/18184-raspberry-pi-cm4-compute-module-4-1gb-ram-16gb-emmc-wifibluetooth-cm4101016-5904422368395.html",
+      sku: "CM4101016"
+    },
+
+    # RPi 3
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/raspberry-pi-3a/13251-raspberry-pi-3-model-a-wifi-dual-band-bluetooth-512mb-ram-14ghz-652508442181.html",
+      sku: "RPI3-MODAP"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/moduly-i-zestawy-raspberry-pi-3b-plus/11142-raspberry-pi-3-model-b-wifi-dualband-bluetooth-1gb-ram-14ghz-713179640259.html",
+      sku: "RPI3-MODBP"
+    },
+
+    # RPi 4
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/moduly-i-zestawy-raspberry-pi-4b/14646-raspberry-pi-4-model-b-wifi-dualband-bluetooth-2gb-ram-15ghz-765756931175.html",
+      sku: "RPI4-MODBP-2GB"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/moduly-i-zestawy-raspberry-pi-4b/14647-raspberry-pi-4-model-b-wifi-dualband-bluetooth-4gb-ram-15ghz-765756931182.html",
+      sku: "RPI4-MODBP-4GB"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/moduly-i-zestawy-raspberry-pi-4b/16579-raspberry-pi-4-model-b-wifi-dualband-bluetooth-8gb-ram-15ghz-765756931199.html",
+      sku: "RPI4-MODBP-8GB"
+    },
+
+    # RPi Zero
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/moduly-i-zestawy-raspberry-pi-zero/8330-raspberry-pi-zero-w-512mb-ram-wifi-bt-41-5904422311513.html",
+      sku: "SC0020"
+    },
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/moduly-i-zestawy-raspberry-pi-zero/9749-raspberry-pi-zero-wh-512mb-ram-wifi-bt-41-ze-zlaczami-5904422305086.html",
+      sku: "SC0020WH"
+    },
+
+    # RPi Zero 2
+    %ProductUpdate{
+      url:
+        "https://botland.com.pl/moduly-i-zestawy-raspberry-pi-zero/20347-raspberry-pi-zero-2-w-512mb-ram-wifi-bt-42.html",
+      sku: "SC0510"
+    }
+  ]
+
+  def all(), do: @items
+end
