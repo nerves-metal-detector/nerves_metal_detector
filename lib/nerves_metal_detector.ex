b6c0adcdee2e0d1882e2c46e0734676d05ca88db
Phillipp Ohlandt <phillipp.ohlandt@googlemail.com>
2023-02-01 20:40:42 +0100

    distribute order of product update jobs to not DOS a vendor

--- lib/nerves_metal_detector.ex
+++ lib/nerves_metal_detector.ex
@@ -19,10 +19,37 @@ defmodule NervesMetalDetector do
 
   def schedule_product_updates(items) do
     items
+    |> distribute_product_update_items()
     |> Enum.map(&ProductUpdate.encode_args/1)
     |> Enum.map(&ProductUpdate.new/1)
     |> Enum.map(&Oban.insert/1)
 
     :ok
+  end
+
+  defp distribute_product_update_items(items) do
+    items
+    |> Enum.chunk_by(fn item ->
+      item.__struct__
+    end)
+    |> interleave()
+  end
+
+  defp interleave(chunks, list \\ []) do
+    {list, chunks} =
+      Enum.reduce(chunks, {list, []}, fn x, {l, c} ->
+        [item | rest] = x
+        new_list = [item | l]
+
+        case rest do
+          [] -> {new_list, c}
+          rest -> {new_list, [rest | c]}
+        end
+      end)
+
+    case chunks do
+      [] -> Enum.reverse(list)
+      chunks -> interleave(Enum.reverse(chunks), list)
+    end
   end
 end
